3dad5f6a1e111fc602e627296444b809fafb15d4
Luiz Mendes <luiz.mendes@in8.com.br>
2021-07-23 11:07:06 -0300

    fix: add version route api name

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express from 'express';
 
 // Controllers routes
 import info from './controllers/info';
+import crawler from './controllers/crawler';
 
 const app = express();
 
 app.set('port', 9000);
 
 app.get('/', info);
+app.get('/v1/crawler', crawler);
 
 export default app;
