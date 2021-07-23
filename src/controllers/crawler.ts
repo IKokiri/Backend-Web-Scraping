21d5a00818acd9a13d02cbe6c2d6a565d3259bbb
Luiz Mendes <luiz.mendes@in8.com.br>
2021-07-23 13:32:38 -0300

    feat: implement get data site

--- src/controllers/crawler.ts
+++ src/controllers/crawler.ts
@@ -1,6 +1,14 @@
 import { Request, Response } from 'express';
+import getPage from '../api/getPage';
 
-const crawler = (req: Request, res: Response) => {
+const crawler = async (req: Request, res: Response) => {
+    
+    const test = await getPage(
+        'https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops',
+    );
+
+    console.log(test);
+
     res.status(200).json({ status: true });
 };
 
