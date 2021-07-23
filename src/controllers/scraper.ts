c5ff733f9fc187d049eda1ba3dce64bf462c94f0
Luiz Mendes <luiz.mendes@in8.com.br>
2021-07-23 17:36:49 -0300

    feat: add a map to list elements

--- src/controllers/scraper.ts
+++ src/controllers/scraper.ts
@@ -1,11 +1,18 @@
 import cheerio from 'cheerio';
 
-const scraper = (html: string): string => {
+const scraper = (html: string): any => {
     const $ = cheerio.load(html);
+    const sectionNotebooks = $('body').find('.thumbnail');
 
-    const teste = $('.thumbnail .price');
-
-    return teste.text();
+    sectionNotebooks.map((i, element) => {
+        console.log($(element).find('.title').attr('title'));
+        console.log($(element).find('.description').text());
+        console.log($(element).find('.ratings p').text());
+        console.log($(element).find('img').attr('src'));
+        console.log($(element).find('.price').text());
+        console.log('****************');
+        return 1;
+    });
 };
 
 export default scraper;
