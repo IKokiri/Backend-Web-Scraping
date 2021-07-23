4605d6404f228d6a0baaa3825d60fd86c84d083d
Luiz Mendes <luiz.mendes@in8.com.br>
2021-07-23 17:36:01 -0300

    refactor: remove avaliation code

--- src/controllers/crawler.ts
+++ src/controllers/crawler.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from 'express';
 import getPage from '../api/getPage';
+import scraper from './scraper';
 
 const crawler = async (req: Request, res: Response) => {
-    const test = await getPage(
+    const html = await getPage(
         'https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops',
     );
 
-    console.log(test);
+    const elements = scraper(html);
 
     res.status(200).json({ status: true });
 };
