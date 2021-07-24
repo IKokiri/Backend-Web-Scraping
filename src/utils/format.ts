1d8e01a8c267eb7639280e4089a38b66bb8275ee
Luiz Mendes <luiz.mendes@in8.com.br>
2021-07-24 09:43:14 -0300

    feat: add format string class

---
+++ src/utils/format.ts
@@ -0,0 +1,13 @@
+class FormatString {
+    term: string;
+
+    constructor(term: string) {
+        this.term = term;
+    }
+
+    removeLastSpaceOfString(): string {
+        return this.term.trim();
+    }
+}
+
+export default Format;
