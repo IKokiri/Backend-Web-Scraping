35306d3a326377d78ba2500a3c105e3d4c877bf3
Luiz Mendes <luiz.mendes@in8.com.br>
2021-07-24 20:09:45 -0300

    chore: page temporarily added until webscraper.io returns

--- src/controllers/crawler.ts
+++ src/controllers/crawler.ts
@@ -4,12 +4,10 @@ import { Notebook } from '../types/notebook';
 import scraper from './scraper';
 
 const crawler = async (req: Request, res: Response) => {
-    const html = await getPage(
-        'https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops',
-    );
+    const html = await getPage('http://localhost:8000/pageMock.html');
 
     const notebooks: Array<Notebook> = scraper(html);
-
+    console.log(notebooks);
     res.status(200).json({ status: notebooks });
 };
 
