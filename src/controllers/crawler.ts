5b47d883d22efd9605da17316296eedf18660efc
Luiz Mendes <luiz.mendes@in8.com.br>
2021-07-25 19:13:24 -0300

    refactor: move script getpage for scraper

--- src/controllers/crawler.ts
+++ src/controllers/crawler.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from 'express';
-import getPage from '../api/getPage';
 import Notebook from '../Entity/Notebook';
 import CreateManyNotebooks from '../services/createManyNotebooks';
-import scraper from './scraper';
+import scraper from '../services/scraper';
 
 const crawler = async (req: Request, res: Response) => {
-    const html = await getPage('http://localhost:8000/pageMock.html');
+    const notebooks: Array<Notebook> = await scraper().then(data => {
+        return data;
+    });
 
-    const notebooks: Array<Notebook> = scraper(html);
     const createManyNotebooks: CreateManyNotebooks = new CreateManyNotebooks();
     createManyNotebooks.create(notebooks);
     res.status(200).json(notebooks);
