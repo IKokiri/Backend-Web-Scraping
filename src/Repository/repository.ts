1dccc8ef92e78189ba8efb17178dcf1963334180
Luiz Mendes <luiz.mendes@in8.com.br>
2021-07-26 13:20:00 -0300

    feat: add get id notebook

--- src/Repository/repository.ts
+++ src/Repository/repository.ts
@@ -1,11 +1,21 @@
+import { getRepository } from 'typeorm';
 import Notebook from '../Entity/Notebook';
 
 class Repository {
     private notebook!: Notebook;
 
+    private notebookDetail = getRepository(Notebook);
+
     create(notebook: Notebook): void {
         this.notebook = notebook;
         Notebook.insert(this.notebook);
+    }
+
+    async getId(id: number): Promise<Notebook | undefined> {
+        const data = await this.notebookDetail.findOne({
+            where: { idNotebook: id },
+        });
+        return data;
     }
 }
 
