f65d9bcff65905ed8f9b97b780eb59458bebfcbe
Luiz Mendes <luiz.mendes@in8.com.br>
2021-07-26 11:57:11 -0300

    feat: add entity to notebook details

---
+++ src/Entity/NotebookDetail.ts
@@ -0,0 +1,35 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    BaseEntity,
+    Unique,
+} from 'typeorm';
+import Notebook from './Notebook';
+
+@Entity()
+@Unique(['idNotebook'])
+class NotebookDetail extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    img!: string;
+
+    @Column()
+    model!: string;
+
+    @Column()
+    description!: string;
+
+    @Column('float')
+    price!: number;
+
+    @Column()
+    ratting!: string;
+
+    @Column()
+    idNotebook!: number;
+}
+
+export default NotebookDetail;
