e0453ed4a50a679c386aa037e947cb55343f1f00
Luiz Mendes <luiz.mendes@in8.com.br>
2021-07-26 09:23:23 -0300

    feat: add filter for model of notebooks

---
+++ src/utils/GetonlyModel.ts
@@ -0,0 +1,14 @@
+import Notebook from '../Entity/Notebook';
+
+const getOnlyModel = (
+    notebooks: Array<Notebook>,
+    model: string,
+): Array<Notebook> => {
+    const notebooksOnlyOneModel: Array<Notebook> = notebooks.filter(not => {
+        return not.model.includes(model);
+    });
+
+    return notebooksOnlyOneModel;
+};
+
+export default getOnlyModel;
