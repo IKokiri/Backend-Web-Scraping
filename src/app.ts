c414ddbfe43f9fb6c68efe9b255898c6b8615d15
Luiz Mendes <luiz.mendes@in8.com.br>
2021-07-27 11:15:49 -0300

    refactor: change name of reference userRoute

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import crawler from './controllers/crawler';
 import Notebook from './Entity/Notebook';
 import crawlerDetails from './controllers/crawlerDetails';
 import User from './User/Entity/User';
-import router from './User/routes';
+import routerUser from './User/routes';
 
 /**
  * database connector
@@ -28,6 +28,6 @@ app.get('/', info);
 app.get('/v1/crawler', crawler);
 app.get('/v1/crawlerDetail/:id', crawlerDetails);
 
-app.use(router);
+app.use(routerUser);
 
 export default app;
