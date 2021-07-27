b79558795c807e676ff5d5a09e03be93d4d7e4e0
Luiz Mendes <luiz.mendes@in8.com.br>
2021-07-27 09:06:58 -0300

    refactor: add repository in constructor

--- src/User/Repository/TypeormUserRepository.ts
+++ src/User/Repository/TypeormUserRepository.ts
@@ -3,7 +3,7 @@ import User from '../Entity/User';
 import { IUserRepository } from './IUserRepository';
 
 class TypeormUserRepository implements IUserRepository {
-    private repository: Repository<User>;
+    constructor(private repository: Repository<User>) {}
 
     create(user: User): Promise<void> {
         this.repository = getRepository(User);
