bfa079dd3911fc4084799a24751702f4b6d0293c
Luiz Mendes <luiz.mendes@in8.com.br>
2021-07-28 03:17:47 -0300

    fix: add properties

--- src/User/Repository/TypeormOrderRepository.ts
+++ src/User/Repository/TypeormOrderRepository.ts
@@ -1,11 +1,12 @@
-import { getRepository } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 import UserOrder from '../Entity/UserOrder';
 import { IOrderRepository } from './IOrderRepository';
 
 class typeormOrderRepository implements IOrderRepository {
-    repository = getRepository(UserOrder);
+    private repository: Repository<UserOrder> | undefined;
 
     create(order: UserOrder): void {
+        this.repository = getRepository(UserOrder);
         this.repository.insert(order);
     }
 }
