1fc32ba0bd1013b942d3bdd07fa5b8403a0143b6
Luiz Mendes <luiz.mendes@in8.com.br>
2021-07-29 11:51:28 -0300

    chore: add initial config of docs

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import { createConnection } from 'typeorm';
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 // Controllers routes
 import Notebook from './Entity/Notebook';
 import User from './Entity/User';
 import UserOrder from './Entity/UserOrder';
 import router from './routes';
-
+import swaggerDocs from './docs/swagger.json';
 /**
  * database connector
  */
@@ -19,7 +20,10 @@ createConnection({
 });
 
 const app = express();
+
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.set('port', 9000);
 
 app.use('/v1', router);
