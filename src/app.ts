84d23f232e8b0b26f5a1c070b6c4d695d4ebeb61
Luiz Mendes <luiz.mendes@in8.com.br>
2021-08-04 19:59:15 -0300

    fix: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { createConnection } from 'typeorm';
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
 
 // Controllers routes
 import Notebook from './Entity/Notebook';
@@ -20,7 +21,7 @@ createConnection({
 });
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
