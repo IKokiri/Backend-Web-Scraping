310cc6109a0f8c53de8d3604872146d77f2cc126
Luiz Mendes <luiz.mendes@in8.com.br>
2021-08-14 15:05:07 -0300

    refactor: import correct entity about notebook

--- src/Utils/SortNotebookPrice.ts
+++ src/Utils/SortNotebookPrice.ts
@@ -1,4 +1,4 @@
-import Notebook from '../Entity/Notebook';
+import Notebook from '../entity/Notebook';
 
 const sortNotebookPrice = (notebooks: Array<Notebook>): Array<Notebook> => {
     const sortedNotebooks: Array<Notebook> = notebooks.sort((obj1, obj2) => {
